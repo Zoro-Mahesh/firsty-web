8cc4a8907388e424ed0abb8e54decfd9cee7286d
ma mahesh <maheshuma147@gmail.com>
2024-07-04 23:15:23 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    info: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    duration: 6,
+    title: "Tibet Adventure",
+    cost: 2100,
+    location: "Tibet Adventure",
+    date: "december 5th, 2019",
+  },
+  {
+    id: 2,
+    image: tour2,
+    info: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    duration: 6,
+    title: "Tibet Adventure",
+    cost: 2100,
+    location: "Tibet Adventure",
+    date: "december 5th, 2019",
+  },
+  {
+    id: 3,
+    image: tour3,
+    info: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    duration: 6,
+    title: "Tibet Adventure",
+    cost: 2100,
+    location: "Tibet Adventure",
+    date: "december 5th, 2019",
+  },
+  {
+    id: 4,
+    image: tour4,
+    info: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    duration: 6,
+    title: "Tibet Adventure",
+    cost: 2100,
+    location: "Tibet Adventure",
+    date: "december 5th, 2019",
+  },
+];
